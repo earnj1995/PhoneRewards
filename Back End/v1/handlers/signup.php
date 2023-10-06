70e4bf7741105305bc184efa4f5344ab7998f43c
Collin Biedenkapp <turbofandude@gmail.com>
2023-10-06 02:07:41 -0400

    feat: welcome message

--- Back End/v1/handlers/signup.php
+++ Back End/v1/handlers/signup.php
@@ -1,7 +1,7 @@
 <?php
 if(!defined('API')) die();
 
-$input = $_GET;
+$input = $_POST;
 
 assertData($input, array(        
     'phone' => 'string',
@@ -24,6 +24,12 @@ if(count($existing) > 0)
 
 if($database->execute('INSERT INTO users (id, phonenumber, name) VALUES (NULL, ?, ?)', array($phone, $input['name'])) === false)
     error(500, 'Failed to add customer');
+
+$user = $database->fetch('SELECT id FROM users WHERE phonenumber = ?', array($phone));
+if(count($user) === 0)
+    error(500, 'Failed to add customer');
+
+$database->execute('INSERT INTO smsjobs (id, userid, message) VALUES (NULL, ?, ?)', array($user[0]['id'], WELCOME_MESSAGE));
 
 echo(json_encode(array('success' => true)));
 ?>
