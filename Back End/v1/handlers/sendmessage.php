3ca80538e860cbea1064c9772e70ea74b9ec134b
Collin Biedenkapp <turbofandude@gmail.com>
2023-12-05 20:25:53 -0500

    feat: prohibited content blocker

--- Back End/v1/handlers/sendmessage.php
+++ Back End/v1/handlers/sendmessage.php
@@ -14,6 +14,54 @@ $userids = $database->fetch('SELECT id FROM users');
 $query = 'INSERT INTO smsjobs (id, userid, message) VALUES ';
 $idx = 0;
 
+$blockedContent = array(
+    'vape',
+    'smoke',
+    'cbd',
+    'flower',
+    'hemp',
+    'kratom',
+    'cigar',
+    'cigarette',
+    'tobacco',
+    'nicotine',
+    'juul',
+    'vapor',
+    'elf',
+    'lost mary',
+    'marijuana',
+    'weed',
+    'thc',
+    'hhc',
+    'delta 8',
+    'delta8',
+    'd8',
+    'delta-8',
+    'delta 10',
+    'delta10',
+    'd10',
+    'delta-10',
+    'delta 9',
+    'delta9',
+    'd9',
+    'delta-9',
+    'delta 11',
+    'delta11',
+    'd11',
+    'delta-11',
+    'hhc',
+    'thc-p',
+    'edible'
+);
+
+$cleanMessage = str_replace('.', '', strtolower($input['message']));
+
+foreach($blockedContent as $blocked)
+{
+    if(stripos($cleanMessage, $blocked) !== false)
+        error(400, 'Message contains blocked content');
+}
+
 $values = array();
 foreach($userids as $userid)
 {
